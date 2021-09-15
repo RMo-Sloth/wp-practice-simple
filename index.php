c105db87e28ed1c0a4c95a6a68e952972a835ff5
Rob Monhemius <robmonhemius@gmail.com>
2021-09-15 06:10:43 +0100

    refactor: printf

--- index.php
+++ index.php
@@ -16,9 +16,12 @@
         while( have_posts() ): 
             the_post();
             the_title('<h3>', '</h3>');
-        ?>
-            <div class="meta">Created By <?php the_author(); ?> on <?php the_time('F j, Y g:i a'); ?>.</div>
-        <?php endwhile;
+            printf( 
+                '<div class="meta">Created By %s on %s.</div>',
+                get_the_author(),
+                get_the_time('F j, Y g:i a')
+            );
+        endwhile;
     else:
         echo wp_autop('Sorry No Posts were found.'); 
     endif; ?>
