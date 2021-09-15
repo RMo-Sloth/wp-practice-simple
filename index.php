e065973de3ac1d4be99454c98c899e15dca16cb3
Rob Monhemius <robmonhemius@gmail.com>
2021-09-15 01:44:52 +0100

    wp_head()

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <?php wp_head(); ?>
     <title><?php bloginfo( 'name' ); ?></title>
 </head>
 <body>
