4e9138de53d89370cd056398e61d4dc8a3411cb8
Rob Monhemius <robmonhemius@gmail.com>
2021-09-15 01:39:06 +0100

    link stylesheet with get_stylesheet_uri()

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <title><?php bloginfo( 'name' ); ?></title>
 </head>
 <body>
