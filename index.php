8647ecc6820fc6adccf03b4b58d3e75709f79aa3
Rob Monhemius <robmonhemius@gmail.com>
2021-09-15 01:29:02 +0100

    ch-3

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <title><?php bloginfo( 'name' ); ?></title>
 </head>
 <body>
