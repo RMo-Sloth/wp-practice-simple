13d4d1e3383bc18b1f0c4548f4dc74bccdbe4ade
Rob Monhemius <robmonhemius@gmail.com>
2021-09-15 09:16:09 +0100

    created primary navigation

--- functions.php
+++ functions.php
@@ -1,10 +1,14 @@
 <?php
 function simple_theme_setup(){
     add_theme_support('post-thumbnails');
+
+    register_nav_menus( [
+        'primary' => __('Primary Menu')
+    ] );
 }
 add_action('after_setup_theme', 'simple_theme_setup');
 
-function set_excerpt_length() {
+function get_excerpt_length() {
     return 25;
 }
-add_action('excerpt_length', 'set_excerpt_length');
+add_action('excerpt_length', 'get_excerpt_length');
--- header.php
+++ header.php
@@ -1,4 +1,14 @@
 <header><div class="container">
     <h1><?php bloginfo('name'); ?></h1>
-    <span><?php bloginfo('description'); ?></span>    
+    <span><?php bloginfo('description'); ?></span>
 </div></header>
+<?php 
+    printf(
+        '<nav class="main-nav">%s</nav>',
+        wp_nav_menu([
+            'theme_location' => 'primary',
+            'container_class' => 'container',
+            'echo' => false
+        ])
+    );
+?>
